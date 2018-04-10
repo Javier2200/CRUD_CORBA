f8732ac6d318c99e7106bc5d8997f921edf3d91f
JONEL <JONEL@JONEL-VAIO>
2018-04-09 19:14:18 -0500

    Primera version

---
+++ Suma _con_Corba/src/suma/_con_corba/Suma_con_Corba.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package suma._con_corba;
+
+/**
+ *
+ * @author JONEL
+ */
+public class Suma_con_Corba {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
